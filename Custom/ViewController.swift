793bc72622bb69bb6ace2e6d2b37bcf72cc5eeff
Asuka Takemura <assan.takemura@gamil.com>
2020-05-23 00:49:39 +0900

    Initial Commit

---
+++ Custom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom
+//
+//  Created by 竹村明日香 on 2020/05/23.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
