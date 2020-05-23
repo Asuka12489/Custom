87083f8c3802285cdaedd748726e9e8c42b2e931
Asuka Takemura <assan.takemura@gamil.com>
2020-05-23 16:42:34 +0900

    initial commit

---
+++ Custom/RemindViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RemindViewController.swift
+//  Custom
+//
+//  Created by 竹村明日香 on 2020/05/23.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class RemindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
